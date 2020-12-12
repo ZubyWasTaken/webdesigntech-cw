b398f30edb4c07f5061bc88cd98502833c4b555c
Zubair <zubairkhalid12@gmail.com>
2020-12-12 03:58:43 +0000

    contact us works, working on job search page

--- WEB-CW/PHP/contactus.php
+++ WEB-CW/PHP/contactus.php
@@ -1,4 +1,26 @@
 <?php
- 	echo 'Hi '.$_POST['name'].' thanks for your message.';
- 	echo 'We will contact you soon';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "database";
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Connected successfully <br />";
+}
+catch(PDOException $e)
+{
+echo "Connection failed: " . $e->getMessage();
+}
+$query=$conn->prepare("INSERT INTO ContactUs (name, email, message) VALUES (?,?,?)");
+$query->bindParam(1, $name);
+$query->bindParam(2, $email);
+$query->bindParam(3, $message);
+$name=$_POST['name'];
+$email=$_POST['email'];
+$message=$_POST['message'];
+$query->execute();
+$conn = null;
+echo 'Hi '.$_POST['name'].' thanks for your interest.</br>';
+echo 'We will contact you at '. $_POST['email'].' soon.</br>';
 ?>
